9596a510e8deeb2634c3286da587b49442431e54
Aidan Blenkinsopp <aidan@eviltwin.co.uk>
2020-11-24 16:56:17 +0000

    update our menu items to navigate

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,28 @@
+import { useHistory, useRouteMatch } from 'react-router-dom';
+
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ imageUrl, linkUrl, size, title }) => {
+    const history = useHistory();
+    const match = useRouteMatch();
+    
+    return (
         <div 
-            className='background-image' 
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }} 
-        />
-        <div className='content'>
-            <h1 className='title'>{title}</h1>
-            <span className='subtitle'>Shop Now</span>
+            className={`${size} menu-item`} 
+            onClick={() => history.push(`${match.url}${linkUrl}`)}
+        >
+            <div 
+                className='background-image' 
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }} 
+            />
+            <div className='content'>
+                <h1 className='title'>{title}</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default MenuItem
